735d4ef1120664113d9da70d98079f5642b1a7e1
Spyroboy26 <163228717+Spyroboy26@users.noreply.github.com>
2026-07-18 04:47:39 -0500

    Update build.gradle.kts

--- src/en/toontop/build.gradle.kts
+++ src/en/toontop/build.gradle.kts
@@ -1,1 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
 
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "ToonTop"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.4"
+
+    source {
+        lang = "en"
+        baseUrl = "https://toontop.io"
+    }
+}
