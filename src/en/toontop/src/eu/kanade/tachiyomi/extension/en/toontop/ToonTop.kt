9f7e10273167509193c30ec5aac0bbd681240fcf
Spyroboy26 <163228717+Spyroboy26@users.noreply.github.com>
2026-07-19 04:32:48 -0500

    Update ToonTop.kt

--- src/en/toontop/src/eu/kanade/tachiyomi/extension/en/toontop/ToonTop.kt
+++ src/en/toontop/src/eu/kanade/tachiyomi/extension/en/toontop/ToonTop.kt
@@ -19,59 +19,55 @@ class ToonTop : ParsedHttpSource() {
     override val lang = "en"
     override val supportsLatest = true
 
-    override val headers = super.headers.newBuilder()
-        .add("Referer", "$baseUrl/")
-        .build()
-
     override fun popularMangaRequest(page: Int): Request =
-        GET("$baseUrl/most-popular?page=$page", headers)
+        GET("$baseUrl/", headers)
 
-    override fun popularMangaSelector() = ".flw-item"
-    override fun popularMangaNextPageSelector() = ".pagination .active + li a"
+    override fun popularMangaSelector() = "div.flw-item"
 
-    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        val link = element.selectFirst(".film-name a")!!
-        setUrlWithoutDomain(link.attr("href"))
-        title = link.text()
-        thumbnail_url = element.selectFirst("img")?.attr("data-src")
-            ?: element.selectFirst("img")?.attr("src")
-    }
+    override fun popularMangaNextPageSelector() = "a.page-link.next"
+
+    override fun popularMangaFromElement(element: Element): SManga =
+        SManga.create().apply {
+            val link = element.selectFirst("a")!!
+            setUrlWithoutDomain(link.attr("href"))
+            title = link.attr("title").ifEmpty { link.text() }
+            thumbnail_url = element.selectFirst("img")?.attr("src")
+        }
 
     override fun latestUpdatesRequest(page: Int): Request =
-        GET("$baseUrl/latest-release?page=$page", headers)
+        popularMangaRequest(page)
 
     override fun latestUpdatesSelector() = popularMangaSelector()
+
     override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
+
+    override fun latestUpdatesFromElement(element: Element) =
+        popularMangaFromElement(element)
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
-        GET("$baseUrl/search?keyword=$query&page=$page", headers)
+        GET("$baseUrl/?s=$query&page=$page", headers)
 
     override fun searchMangaSelector() = popularMangaSelector()
+
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
-    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
 
-    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
-        title = document.selectFirst("h1, .film-name")?.text() ?: "Unknown"
-        description = document.selectFirst(".film-description, .description")?.text()
-        genre = document.select(".item-list a").joinToString { it.text() }
-        thumbnail_url = document.selectFirst("img.film-poster-img, .film-poster img")?.attr("src")
-    }
+    override fun searchMangaFromElement(element: Element) =
+        popularMangaFromElement(element)
 
-    override fun chapterListSelector() = ".c-item a, ul.episodes-list li a"
+    override fun mangaDetailsParse(document: Document): SManga =
+        SManga.create()
 
-    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        setUrlWithoutDomain(element.attr("href"))
-        name = element.selectFirst(".name, .chapter-name")?.text() ?: element.text()
-    }
+    override fun chapterListSelector() = "a.chapter"
 
-    override fun pageListParse(document: Document): List<Page> {
-        return document.select(".read-content img, .reader-area img, .chapter-images img")
-            .mapIndexed { index, element ->
-                val imageUrl = element.attr("data-src").ifEmpty { element.attr("src") }
-                Page(index, "", imageUrl)
-            }
-    }
+    override fun chapterFromElement(element: Element): SChapter =
+        SChapter.create().apply {
+            setUrlWithoutDomain(element.attr("href"))
+            name = element.text()
+        }
 
-    override fun imageUrlParse(document: Document): String = throw UnsupportedOperationException()
+    override fun pageListParse(document: Document): List<Page> =
+        emptyList()
+
+    override fun imageUrlParse(document: Document): String =
+        throw UnsupportedOperationException()
 }
