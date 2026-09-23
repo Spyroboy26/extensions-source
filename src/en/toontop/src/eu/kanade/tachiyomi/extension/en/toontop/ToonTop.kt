4b0e6a3532ebb49a63677d2a80b4f0d246f7a482
Spyroboy26 <163228717+Spyroboy26@users.noreply.github.com>
2026-09-23 15:51:55 -0500

    Update ToonTop.kt

--- src/en/toontop/src/eu/kanade/tachiyomi/extension/en/toontop/ToonTop.kt
+++ src/en/toontop/src/eu/kanade/tachiyomi/extension/en/toontop/ToonTop.kt
@@ -19,58 +19,49 @@ class ToonTop : ParsedHttpSource() {
     override val lang = "en"
     override val supportsLatest = true
 
-    override fun popularMangaRequest(page: Int): Request =
-        GET("$baseUrl/", headers)
+    override val headers = super.headers.newBuilder()
+        .add("Referer", "$baseUrl/")
+        .build()
 
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/most-popular?page=$page", headers)
     override fun popularMangaSelector() = "div.flw-item"
+    override fun popularMangaNextPageSelector() = "li.page-item.active + li a"
 
-    override fun popularMangaNextPageSelector() = "a.page-link.next"
+    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
+        val link = element.selectFirst("a")!!
+        setUrlWithoutDomain(link.attr("href"))
+        title = link.attr("title").ifEmpty { link.text() }
+        thumbnail_url = element.selectFirst("img")?.attr("data-src") ?: element.selectFirst("img")?.attr("src")
+    }
 
-    override fun popularMangaFromElement(element: Element): SManga =
-        SManga.create().apply {
-            val link = element.selectFirst("a")!!
-            setUrlWithoutDomain(link.attr("href"))
-            title = link.attr("title").ifEmpty { link.text() }
-            thumbnail_url = element.selectFirst("img")?.attr("src")
-        }
-
-    override fun latestUpdatesRequest(page: Int): Request =
-        popularMangaRequest(page)
-
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/latest-release?page=$page", headers)
     override fun latestUpdatesSelector() = popularMangaSelector()
-
     override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun latestUpdatesFromElement(element: Element) =
-        popularMangaFromElement(element)
+    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
-        GET("$baseUrl/?s=$query&page=$page", headers)
+        GET("$baseUrl/search?keyword=$query&page=$page", headers)
 
     override fun searchMangaSelector() = popularMangaSelector()
-
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
+    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
 
-    override fun searchMangaFromElement(element: Element) =
-        popularMangaFromElement(element)
-
-    override fun mangaDetailsParse(document: Document): SManga {
-    return SManga.create().apply {
-        title = name
-    }
+    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+        title = document.selectFirst("h1, h2.film-name")?.text() ?: name
     }
 
-    override fun chapterListSelector() = "a.chapter"
+    override fun chapterListSelector() = "a.chapter, .c-item a"
+    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
+        setUrlWithoutDomain(element.attr("href"))
+        name = element.text()
+    }
 
-    override fun chapterFromElement(element: Element): SChapter =
-        SChapter.create().apply {
-            setUrlWithoutDomain(element.attr("href"))
-            name = element.text()
+    override fun pageListParse(document: Document): List<Page> {
+        return document.select("img").mapIndexed { index, element ->
+            val url = element.attr("data-src").ifEmpty { element.attr("src") }
+            Page(index, imageUrl = url)
         }
+    }
 
-    override fun pageListParse(document: Document): List<Page> =
-        emptyList()
-
-    override fun imageUrlParse(document: Document): String =
-        throw UnsupportedOperationException()
+    override fun imageUrlParse(document: Document): String = throw UnsupportedOperationException()
 }
